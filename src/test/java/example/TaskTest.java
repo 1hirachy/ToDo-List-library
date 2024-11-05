230e8b874d8a6d2a1c95decc7254e059db39cbf6
MD Chowdhury <you@example.com>
2024-11-05 15:05:45 -0500

    Completed test for task

---
+++ src/test/java/example/TaskTest.java
@@ -0,0 +1,22 @@
+package example;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TaskTest {
+
+    @Test
+    void setDescription_shouldSetDescription_correctly() {
+        Task task = new Task("Initial  Task");
+        task.setDescription("Updated Task");
+        assertEquals("Updated Task", task.getDescription());
+    }
+
+    @Test
+    void setComplete_shouldSetCompleteStatus() {
+        Task task = new Task("Sample Task");
+        task.setComplete(true);
+        assertTrue(task.isComplete());
+    }
+
+}
